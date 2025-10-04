257332be96081db200f56f9ac00a0704fce59cd8
gc85-ua <gc85-ua@alu.ua.es>
2025-10-04 18:26:22 +0200

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.routes.ml_route import router as ml_router
+# allow CORS for all origins (for development purposes)
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(ml_router)
 @app.get("/")
 async def read_root():
