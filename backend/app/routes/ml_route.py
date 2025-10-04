558c27005a45ad1a5855e20203c83a059177cbe1
gc85-ua <gc85-ua@alu.ua.es>
2025-10-05 00:58:09 +0200

    add data to be shown by api

--- backend/app/routes/ml_route.py
+++ backend/app/routes/ml_route.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Query, Response
+from fastapi import APIRouter, Query, Response, Body
+from pydantic import BaseModel
 
 import joblib,json
 import numpy as np
@@ -8,13 +9,24 @@ model = joblib.load("./routes/random_forest_model.joblib")
 
 router = APIRouter()
 
-@router.post("/predict")
-async def predict(body: dict):
-    orbit_period_days = float(body.get("orbit_period_days"))
-    stellar_radius_sun = float(body.get("stellar_radius_sun"))
-    stellar_effective_temp_kelvin = float(body.get("stellar_effective_temp_kelvin"))
-    planet_equilibrium_temp_kelvin = float(body.get("planet_equilibrium_temp_kelvin"))
-    stellar_surface_gravity_log10_cm_s2 = float(body.get("stellar_surface_gravity_log10_cm_s2"))
+class PredictionRequest(BaseModel):
+    orbit_period_days: float
+    stellar_radius_sun: float
+    stellar_effective_temp_kelvin: float
+    planet_equilibrium_temp_kelvin: float
+    stellar_surface_gravity_log10_cm_s2: float
+
+@router.post("/predict",
+             summary="Predicts exoplanet candidates",
+             description="Predicts whether a planet is an exoplanet candidate using the trained Random Forest model. The input features are: orbit_period_days, stellar_radius_sun, stellar_effective_temp_kelvin, planet_equilibrium_temp_kelvin, stellar_surface_gravity_log10_cm_s2.",
+             response_description="Prediction result indicating whether the planet is a CANDIDATE or NOT A CANDIDATE.",
+             )
+async def predict(body: PredictionRequest):
+    orbit_period_days = body.orbit_period_days
+    stellar_radius_sun = body.stellar_radius_sun
+    stellar_effective_temp_kelvin = body.stellar_effective_temp_kelvin
+    planet_equilibrium_temp_kelvin = body.planet_equilibrium_temp_kelvin
+    stellar_surface_gravity_log10_cm_s2 = body.stellar_surface_gravity_log10_cm_s2
     if None in [orbit_period_days, stellar_radius_sun, stellar_effective_temp_kelvin, planet_equilibrium_temp_kelvin, stellar_surface_gravity_log10_cm_s2]:
         return Response(content=json.dumps({"error": "Missing one or more required features in the request body."}), status_code=400, media_type="application/json")
     
